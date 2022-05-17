650de378720c01be972184768dccc12438bbb2bc
Gabriel José <punkj.ramone@live.com>
2022-05-17 09:37:23 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
-import Script from 'next/script'
 import { useRouter } from 'next/router'
 import * as gtag from '../../lib/gtag'
 import { Header } from '../components/header'
+import Analytics from '../service/googleanalytics'
 
 import '../styles/global.scss';
 
@@ -22,27 +22,9 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Header/>
       <Component {...pageProps} />
+      <Analytics/>
     </>
   )
 }
