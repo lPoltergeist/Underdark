2e2de4022b165ed72015f804f7f32ce78035ae35
Gabriel <gbmagalhaes1996@gmail.com>
2025-11-19 10:31:18 -0300

    correção no import da logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,8 @@
 import styles from './styles.module.scss'
 //import DarkMode from '../theme/changetheme'
 
-import {BiMenuAltRight, BiWindows} from 'react-icons/bi'
-import {MdOutlineClose} from 'react-icons/md'
+import { BiMenuAltRight, BiWindows } from 'react-icons/bi'
+import { MdOutlineClose } from 'react-icons/md'
 
 import { useState } from 'react'
 import { useEffect } from 'react';
@@ -12,69 +12,69 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export function Header() {
-const [menuOpen, setmenuOpen] = useState(false);
-const [size, setSize] = useState({
-    width: undefined,
-    height: undefined,
-});
+    const [menuOpen, setmenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
 
-useEffect(() => {
-    const handleResize = () => {
-        setSize({
-            width: window.innerWidth,
-            height: window.innerHeight,
-        })
-    };
-    window.addEventListener("resize", handleResize);
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            })
+        };
+        window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
 
-}, []);
+    }, []);
 
-useEffect(() => {
-    if( size.width > 768 && menuOpen) {
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setmenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+
+    const menuToggleHandler = () => {
+        setmenuOpen((p) => !p);
+    }
+
+    const handleCloseMenuOnClick = () => {
         setmenuOpen(false);
     }
-}, [size.width, menuOpen]);
-
-const menuToggleHandler = () => {
-setmenuOpen((p) => !p);
-}
-
-const handleCloseMenuOnClick = () => {
-    setmenuOpen(false);
-}
 
 
 
-    return(
+    return (
         <header className={styles.headerContainer}>
-                <div className={styles.header}>
+            <div className={styles.header}>
                 <Link href="/posts">
-                <Image className={styles.logoContent} src={logo} alt="/Underdark"   width='210px' height='200px'/>
+                    <Image className={styles.logoContent} src="/logo.png" alt="/Underdark" width='210px' height='200px' />
                 </Link>
                 <nav className={`${styles.navContent} ${menuOpen ? styles.isMenu : ""}`}>
-               <ul>
-                   <li>
-                       <Link href="/">
-                          <a onClick={handleCloseMenuOnClick}>
-                              Home
-                          </a> 
-                           </Link>
-                   </li>
-                   <li>
-                       <Link prefetch href="/posts">
-                           <a onClick={handleCloseMenuOnClick}> 
-                           Posts
-                           </a>
-                           </Link>
-                   </li>
-                   <li>   
-                   </li>
-               </ul>
+                    <ul>
+                        <li>
+                            <Link href="/">
+                                <a onClick={handleCloseMenuOnClick}>
+                                    Home
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link prefetch href="/posts">
+                                <a onClick={handleCloseMenuOnClick}>
+                                    Posts
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                        </li>
+                    </ul>
                 </nav>
                 <div className={styles.headerContentToggle}>
-                   {menuOpen ?  <MdOutlineClose onClick={menuToggleHandler}/>  : <BiMenuAltRight onClick={menuToggleHandler}/>} 
+                    {menuOpen ? <MdOutlineClose onClick={menuToggleHandler} /> : <BiMenuAltRight onClick={menuToggleHandler} />}
                 </div>
             </div>
         </header>
